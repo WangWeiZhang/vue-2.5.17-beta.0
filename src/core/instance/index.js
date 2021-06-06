582b7888ef67baafd9e308e27b87b22d367bf8e1
wangwz10 <wangwz10@lenovo.com>
2021-06-06 15:56:29 +0800

    feat 增加注释

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Description: Vue根文件
+ * @Author: wangwz10@lenovo.com
+ * @LastEditors: wangwz10@lenovo.com
+ * @Date: 2021-06-06 15:54:26
+ * @LastEditTime: 2021-06-06 15:55:13
+ * @FilePath: \vue-2.5.17-beta.0\src\core\instance\index.js
+ */
 import { initMixin } from './init'
 import { stateMixin } from './state'
 import { renderMixin } from './render'
@@ -29,7 +37,7 @@ export default Vue
 /**
  * 为什么不用es6来实现类的定义，而选择使用es5来实现？
  *    因为es6比较难写，而es5可以使用prototype上绑定好多对象和方法。并且可以把方法和对象拆分到不同的文件下，方便代码的管理，不用在一个大文件下，把所有部分都定义在一块。
- * 方便代码的维护和可读。
+ * 方便代码的管理与维护。
  * 
  * 总结：所以Vue本身就是一个函数，是用函数实现的一个类，类上挂载了很多原型和方法
  */
